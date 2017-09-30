a6a7837b1d9c2f332f9667c46ef59ee16186aa11
joaomarcos <jmcosta944@gmail.com>
2017-09-29 21:53:45 -0300

    att

--- fseries/setup.py
+++ fseries/setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
      name='python-fourier-series-calculator',    # This is the name of your PyPI-package.
-     version='0.1.3',                          # Update the version number for new releases
-     scripts=['pyfseries.py'],                  # The name of your scipt, and also the command you'll be using for calling it
+     version='0.1.4',                          # Update the version number for new releases
+     scripts=['fseries.py'],                  # The name of your scipt, and also the command you'll be using for calling it
      author= 'Joao Marcos Costa',
      author_email='jmcosta944@gmail.com',
      keywords='fourier series calculus science engineering math',
